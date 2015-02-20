eac2296d7fc1f2384826d73c9cd92a69ca4ee9ab
flaskalobet <flask.alobet@gmail.com>
2015-02-20 10:22:41 +0700

    add config file

--- structure_flask/config.py
+++ structure_flask/config.py
@@ -8,3 +8,24 @@ class Config:
     @staticmethod
     def init_app(app):
         pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or\
+            'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+#class TestingConfig(Config):
+#    TESTING = True
+#    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or\
+#            'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+#class ProductionConfig(Config):
+#    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
+#            'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+        'default': DevelopmentConfig
+        }
